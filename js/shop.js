c79454c62e8fed8e14a3d7e9d9af8eecef286b3c
Pabl0Parra <pabsblue16@gmail.com>
2022-04-20 22:48:26 +0200

    Started ex 4

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,5 @@
-// If you have time, you can move this variable "products" to a json file and load the data in this js. It will look more professional
-var products = [
+// If you have time, you can move this letiable "products" to a json file and load the data in this js. It will look more professional
+let products = [
   {
     id: 1,
     name: "cooking oil",
@@ -64,38 +64,23 @@ var products = [
   },
 ];
 // Array with products (objects) added directly with push(). Products in this array are repeated.
-var cartList = [];
+let cartList = [];
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-var cart = [];
+let cart = [];
 
-var total = 0;
+let total = 0;
 
 // Exercise 1
 function buy(id) {
   // 1. Loop for to the array products to get the item to add to cart
   // 2. Add found product to the cartList array
-
-  //SECOND ATTEMPT - WORKS FOR EX 3 BUT CARTLIST ARRAY ONLY HAS PRICE
   for (i = 0; i < products.length; i++) {
     if (id == products[i].id) {
-      cartList.push(products[i].price);
+      cartList.push(products[i]);
+      console.log(cartList);
     }
   }
-
-  //FIRST ATTEMPT-WORKING BUT NOT AN ARRAY WITH KEY PAIRS- CAN´T MAKE IT WORK IN EX 3 I BELIEVE
-  // let x = false;
-  // let i = 0;
-
-  // while (x != true) {
-  //   if (id == products[i].id) {
-  //     x = true;
-  //     cartList.push(products[i].name, products[i].price);
-  //   } else {
-  //     i++;
-  //   }
-  // }
-  console.log(cartList);
 }
 
 // Exercise 2
@@ -106,18 +91,33 @@ function cleanCart() {
 // Exercise 3
 function calculateTotal() {
   // Calculate total price of the cart using the "cartList" array
-  let totalPrice = 0;
-
   for (let i = 0; i < cartList.length; i++) {
-    totalPrice += cartList[i];
+    total += cartList[i].price;
   }
-  return `The total price is $${totalPrice}`;
+  return `The total price is $${total}`;
 }
 
 // Exercise 4
 function generateCart() {
   // Using the "cartlist" array that contains all the items in the shopping cart,
   // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+
+  cart = []; //so that every time the function is run, the cart is empty
+  console.log(`Cart beginning state: ${cart}`);
+
+  for (let i = 0; i < cartList.length; i++) {
+    let chosenProduct = cartList[i]; // declare a variable with the item that has been selected
+
+    if (cart.includes(chosenProduct)) {
+      //if the selected item is already in cart...
+      let itemAlreadyInCart = cart.find((x) => x.id == chosenProduct.id); //find the item in cart and save it in a variable
+      itemAlreadyInCart.quantity++; //since it´s already in cart, we have to increase its quantity by one
+    } else {
+      chosenProduct.quantity = 1;
+      cart.push(chosenProduct);
+    }
+  }
+  console.log(`Cart after shopping: ${cart}`);
 }
 
 // Exercise 5
