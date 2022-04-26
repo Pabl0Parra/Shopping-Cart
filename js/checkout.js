335c61f040d7e7304cd0f12e81bfb48daa02b2d7
Pabl0Parra <98957023+Pabl0Parra@users.noreply.github.com>
2022-04-26 13:14:25 +0200

    fixed password validation ex_6

--- js/checkout.js
+++ js/checkout.js
@@ -45,21 +45,21 @@ function validate(event) {
   // Last Name
     // Check if the last name is long enough
     if (lastName.value.length < 3) {
-      errorLastN.classList.add('d-block');
-      errorLastN.classList.add('invalid-feedback');
+      errorLastName.classList.add('d-block');
+      errorLastName.classList.add('invalid-feedback');
       lastName.classList.add('border-danger')
     }
     // Check if it contains numbers 
     if (lastName.value.match(/\d+/g) != null) {
-      errorName.classList.add('d-block');
-      errorName.classList.add('invalid-feedback');
-      firstName.classList.add('border-danger');
+      errorLastName.classList.add('d-block');
+      errorLastName.classList.add('invalid-feedback');
+      errorLastName.classList.add('border-danger');
       
     }
     // Hide error message if the above checks have been passed
     if (lastName.value.length >= 3 && lastName.value.match(/\d+/g) == null) {
-      errorName.classList.remove('d-block')
-      lastName.classList.remove('border-danger'); 
+      errorLastName.classList.remove('d-block')
+      errorLastName.classList.remove('border-danger'); 
     }
 
   // Email
@@ -67,11 +67,11 @@ function validate(event) {
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value) == false) {
       errorEmail.classList.add('d-block');
       errorEmail.classList.add('invalid-feedback');
-      email.classList.add('border-danger');
+      errorEmail.classList.add('border-danger');
     }
     // Hide error message if format is valid 
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
-      errorPassword.classList.remove('d-block')
+      errorEmail.classList.remove('d-block')
       errorEmail.classList.remove('border-danger');     
     }
 
@@ -80,15 +80,13 @@ function validate(event) {
     if (password.value.length < 3) {
       errorPassword.classList.add('d-block');
       errorPassword.classList.add('invalid-feedback');
-      password.classList.add('border-danger');
-      
+      errorPassword.classList.add('border-danger');      
     }
     // Check if it contains letters
     if (password.value.match(/[A-Za-z]/g) != null) {
       errorPassword.classList.add('d-block');
       errorPassword.classList.add('invalid-feedback');
-      password.classList.add('border-danger');
-      
+      password.classList.add('border-danger');      
     }
     // Check if it contains numbers
     if (password.value.match(/\d+/g) != null) {
